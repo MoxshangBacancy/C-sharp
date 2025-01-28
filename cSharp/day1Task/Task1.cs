4b6a1ac67a16899e844f013ab7f6e3e2b1faf2f3
MoxshangBacancy <moxshang.shah@bacancy.com>
2025-01-28 11:20:05 +0530

    Update Task1.cs

--- cSharp/day1Task/Task1.cs
+++ cSharp/day1Task/Task1.cs
@@ -1,1 +1,21 @@
+﻿using System;
 
+namespace Task1
+{
+    public class day
+    {
+        public static int CalculateSumOfDigits(int number)
+        {
+            int sum = 0;
+            int temp = Math.Abs(number); 
+
+            while (temp != 0)
+            {
+                sum += temp % 10;
+                temp /= 10;     
+            }
+
+            return sum;
+        }
+    }
+}
