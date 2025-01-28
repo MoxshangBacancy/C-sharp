511a3204f9d9f5e3934999cb716740246a5fdf1b
MoxshangBacancy <moxshang.shah@bacancy.com>
2025-01-28 11:22:43 +0530

    Update Program.cs

--- cSharp/day2task/Program.cs
+++ cSharp/day2task/Program.cs
@@ -1,1 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string input = Console.ReadLine();
+
+        string reversedSentence = ReverseWords(input);
+        Console.WriteLine("Reversed Sentence: " + reversedSentence);
+    }
+
+    static string ReverseWords(string sentence)
+    {
+        string[] words = sentence.Split(' ');
+        Array.Reverse(words);
+        return string.Join(" ", words);
+    }
+}
 
