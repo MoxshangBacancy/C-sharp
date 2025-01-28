ae0bcbb9bd4876f2087407a80cd67b84f3ed91a2
MoxshangBacancy <moxshang.shah@bacancy.com>
2025-01-28 11:20:36 +0530

    Create program.cs

---
+++ cSharp/day1Task/program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Task1; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number: ");
+
+        int number = int.Parse(Console.ReadLine());
+
+
+        int sum = day.CalculateSumOfDigits(number);
+
+   
+        Console.WriteLine($"The sum of the digits of {number} is: {sum}");
+    }
+}
