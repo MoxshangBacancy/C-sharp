4f9c52730e40aa65388f58b9e4cd5c76967889f0
MoxshangBacancy <moxshang.shah@bacancy.com>
2025-02-09 12:23:14 +0530

    Update PartialClass1.cs

--- cSharp/day7(OOPS2)/PartialClass1.cs
+++ cSharp/day7(OOPS2)/PartialClass1.cs
@@ -1,27 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using HMS;
+
 using HMS;
 
 namespace task7
 {
- 
-   public partial class PatientStatus
-        {
-            public List<Patient> PatientNameList = new List<Patient> { };
-            public void AdmitPatient(Patient PatientName)
-            {
-                PatientNameList.Add(PatientName);
 
-                Console.WriteLine($"Patient {PatientName} admitted successfully.");
-            }
-            public void DischargePatient(Patient PatientName)
-            {
-                PatientNameList.Remove(PatientName);
-                Console.WriteLine($"Patient {PatientName} discharged successfully.");
-            }
+    partial class PatientStatusC
+    {
+        List<Patient> PatientNameLista = new List<Patient> { };
+        public void AdmitPatient(Patient PatientName)
+        {
+            PatientNameLista.Add(PatientName);
+
+            Console.WriteLine($"Patient {PatientName} admitted successfully.");
         }
+        public void DischargePatient(Patient PatientName)
+        {
+            PatientNameLista.Remove(PatientName);
+            Console.WriteLine($"Patient {PatientName} discharged successfully.");
+        }
+    }
 
 }
